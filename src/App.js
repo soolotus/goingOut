992a37f91c668e09b7e8cdbecf28bab838949983
soolotus <soolotus632@gmail.com>
2023-10-06 23:31:15 -0600

    component-Friend

--- src/App.js
+++ src/App.js
@@ -29,17 +29,33 @@ export default function App() {
   );
 }
 function FriendsList() {
+  const friends = initialFriends;
   return (
     <ul>
-      <Friend />
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
     </ul>
   );
 }
-function Friend() {
+function Friend({ friend }) {
   return (
-    <div>
-      <li>friend</li>
-      <button>Add</button>
-    </div>
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          {" "}
+          You owe {friend.name} ${friend.balance * -1}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
   );
 }
