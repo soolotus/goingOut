2d58d206615f8785ccc6e943878cc2668dc25188
soolotus <soolotus632@gmail.com>
2023-10-07 09:29:16 -0600

    Static parts

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -55,7 +58,40 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>😍 Friend Name</label>
+      <input type="text" />
+      <label>Image URL</label>
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>SPLIT a bill with</h2>
+      <label>Bill Value</label>
+      <input type="text" />
+      <label>Your expense</label>
+      <input type="text" />
+      <label>X's expense</label>
+      <input type="text" />
+      <label>Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+    </form>
   );
 }
